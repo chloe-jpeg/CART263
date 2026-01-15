a53e30cc7a536be8cb04a64c7302a673230e4b90
chloe-jpeg <chloe.guerin@gmail.com>
2026-01-15 14:38:44 -0500

    q1

--- task_1/js/script.js
+++ task_1/js/script.js
@@ -2,9 +2,34 @@
 
 function setup() {
     console.log("go")
+    createCanvas(600, 600)
 
 }
 
 function draw() {
+    background(50, 100, 200)
+
+    drawCircle()
+
+}
+
+function drawCircle() {
+    push()
+    noStroke()
+    fill(200, 180, 100)
+    ellipse(20, 20, 20)
+    pop()
+
+    push()
+    noStroke()
+    fill(200, 50, 150)
+    ellipse(50, 50, 40)
+    pop()
+
+    push()
+    noStroke()
+    fill(125, 200, 50)
+    ellipse(100, 100, 80)
+    pop()
 
 }
