c00886cdb2427b83696325dfdddd48ab88becae0
chloe-jpeg <chloe.guerin@gmail.com>
2026-03-29 16:49:09 -0400

    rest of class work

--- class10_threejs/main.js
+++ class10_threejs/main.js
@@ -21,6 +21,29 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
 
 camera.position.z = 3
+
+
+//TURN ON AXES HELPER
+//https://threejs.org/docs/?q=Axes#AxesHelper
+const axesHelper = new THREE.AxesHelper(1)
+scene.add(axesHelper)
+//move it 
+axesHelper.position.x = -1;
+axesHelper.position.y = -1;
+
+const mesh_2 = new THREE.Mesh(geometry, material)
+scene.add(mesh_2)
+mesh_2.position.x = 1.5
+mesh_2.position.y = 1.25
+mesh_2.position.z = -1
+
+mesh.scale.x = 2
+mesh.scale.y = 0.25
+mesh.scale.z = 0.5
+
+
+
+
+renderer.render(scene, camera)
